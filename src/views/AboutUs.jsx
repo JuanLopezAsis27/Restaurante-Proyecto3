2696bcd5855da02d920625e49542f0c1c1200ea5
JuanLopezAsis27 <juan.lopezasis27@hotmail.com>
2024-09-03 20:21:34 -0300

    texto en quienes somos

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div className='flex items-center justify-center my-36 px-20'>
-      <div className='bg-zinc-800 w-full rounded-md min-w-md text-center p-10'>
-        <h1 className='text-8xl mb-5'><b>¿Quienes Somos?</b></h1>
-        <h3 className='text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla autem beatae aperiam odio voluptates dolorem, dolore dicta necessitatibus harum perspiciatis voluptatem similique, fugiat obcaecati consequuntur labore soluta delectus molestiae?
-        Cupiditate asperiores quisquam repellat incidunt, sunt accusantium possimus explicabo minus suscipit ratione aut. Possimus incidunt ab velit rerum soluta impedit ipsam, enim sequi dolor praesentium voluptas nihil? Odio, commodi ut!
-        Aspernatur omnis eius, labore ipsam vitae et sunt quibusdam ipsa deserunt enim recusandae unde, fuga nostrum necessitatibus atque blanditiis temporibus in pariatur architecto possimus suscipit veritatis quo? Odio, earum quos?
-        Quae non temporibus laudantium autem esse incidunt earum molestias, eius ullam. Odio ea adipisci perferendis placeat dolorum est! Adipisci, maxime! Veritatis voluptatibus aliquam provident magni dolore, vel culpa qui voluptas?
-        Aliquid, accusantium? Dolor placeat voluptatibus deserunt cumque dolore similique eum iste soluta veritatis deleniti sed excepturi aperiam molestiae, saepe commodi suscipit architecto odio fugiat doloribus earum. Distinctio officia commodi adipisci.</h3>
+    <div className='flex items-center  my-36 px-20'>
+      <div className='bg-zinc-800 w-full rounded-md min-w-md p-10'>
+        <h1 className='text-8xl mb-10 text-center'><b>¿Quienes Somos?</b></h1>
+        <h3 className='text-2xl px-40'>Somos un restaurante el cual permite reservar mesas a traves de esta pagina. 
+          Para ello solo debes registrarte y colocar El dia y la hora en la cual quieres realizar dicha reserva</h3>
+        <h2 className='text-5xl px-40 py-12'><b>Pagina realizada por:</b></h2> 
+        <p className='px-40 text-xl'><b>-Lopez Asis Juan Manuel: 21 años, estudiante de ingenieria en sistemas de informacion en UTN    </b></p> 
       </div>
     </div>
   )
